636574e1b0e7bd0b557c2a56d639e3cd9c8f2d7a
meraiianass <48887575+meraiianass@users.noreply.github.com>
2021-07-08 13:55:44 +0200

    1st commit on main

---
+++ Main.tf
@@ -0,0 +1,35 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.52.0"
+    }  
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "myrg" {
+  name     = "Anass-rg"
+  location = "West EU"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "Anass-vnet"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create a subnet within the vnet
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "Anass-subnet"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+}
